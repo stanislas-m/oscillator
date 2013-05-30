97265ffffbd1f6d3a25a31146cd52f4df27d87b7
Michalak Stanislas <michalakst@pc216-03.etude.cergy.eisti.fr>
2013-05-30 13:30:20 +0200

    Menu principal presque terminé. Il reste les images à intégrer et les envents à gérer.

--- main.ml
+++ main.ml
@@ -16,8 +16,6 @@ let _ = GMain.init ()
 (**
 * Accesseurs 
 *)
-let test () = print_endline "Click";
-flush stdout
 
 (**
 * Gestion de l'affichage
@@ -40,9 +38,19 @@ let mainTitle = GMisc.label ~markup:"<span font_size='xx-large'><b>Bienvenue !</
 
 let subTitle = GMisc.label ~markup:"<span font_size='large'>Veuillez choisir un type d'oscillateur :</span>" ~packing:vbox#add ()
 
-
-let menuFrame = GBin.frame ~label:"Oscillateurs disponibles"
-    ~packing:(vbox#pack ~expand:true ~fill:true ~padding:10) ~height:300 ()
+let frame = GBin.frame ~height:300 ~label:"Oscillateurs disponibles" ~packing:vbox#add ()
+let frameContent = GPack.hbox ~spacing:10 ~packing:frame#add ()
+let menuMeca = GPack.vbox ~spacing:10 ~packing:frameContent#add ()
+let menuElec = GPack.vbox ~spacing:10 ~packing:frameContent#add () 
+let buttonM = GButton.button
+	~label:"Mécanique"
+	~packing:menuMeca#add ()
+let imgMeca = GMisc.label ~text:"Img meca" ~height:240 ~packing:menuMeca#add ()
+let buttonE = GButton.button
+	~label:"Électrocinétique"
+	~packing:menuElec#add ()
+let imgElec = GMisc.label ~text:"Img elec" ~height:240 ~packing:menuElec#add ()
+let quitButton = GButton.button ~label:"Quitter" ~packing:vbox#add ()
 let _ =
   window#connect#destroy ~callback:GMain.quit;
   window#show ();
