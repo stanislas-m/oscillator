4a682bebb29e0d29ff6cfaa6766d032e3af3bbf9
Stanislas Michalak <stanislas.michalak@gmail.com>
2013-05-31 00:09:06 +0200

    La fenêtre d'accueil fonctionne bien. Il manque les deux illustrations (des types d'oscillateurs). Début d'écriture des launchers.

--- main.ml
+++ main.ml
@@ -14,8 +14,18 @@ let _ = GMain.init ()
 *)
 
 (**
-* Accesseurs 
+* Launchers 
 *)
+
+let startMecaConfig window =
+  let dialogBox = GWindow.dialog ~title:"Configuration d'un oscillateur mécanique" ~height:360 ~width:600 ~parent:window ~destroy_with_parent:true () in  
+  dialogBox#show ()
+;;
+
+let startElecConfig window =
+  print_endline "Affichage de la configuration pour l'oscillateur électrocinétique";
+  flush stdout
+;;
 
 (**
 * Gestion de l'affichage
@@ -25,33 +35,45 @@ let _ = GMain.init ()
 let window = GWindow.window 
   ~title:"Oscillateur mécanique et électrocinétique" 
   ~height:460 
-  ~width:800 ()
+  ~width:800 ();;
 
 (* GTK ne permet pas d'avoir plus d'un fils pour window,
 on créé donc un conteneur pour nos widgets *)
 let vbox = GPack.vbox 
   ~spacing:10
   ~border_width:10
-  ~packing:window#add ()
+  ~packing:window#add ();;
 
-let mainTitle = GMisc.label ~markup:"<span font_size='xx-large'><b>Bienvenue !</b></span>" ~packing:vbox#add ()
+(* Menu principal *)
+let menu window vbox =
+   GMisc.label ~markup:"<span font_size='xx-large'><b>Bienvenue !</b></span>" ~packing:vbox#add ();
+   GMisc.label ~markup:"<span font_size='large'>Veuillez choisir un type d'oscillateur :</span>" ~packing:vbox#add ();
+   let frame = GBin.frame ~height:300 ~label:"Oscillateurs disponibles" ~packing:vbox#add () in
+      let frameContent = GPack.hbox ~spacing:10 ~packing:frame#add () in
+         let menuMeca = GPack.vbox ~spacing:10 ~packing:frameContent#add () in
+            let buttonM = GButton.button
+	         ~label:"Mécanique"
+	         ~packing:menuMeca#add () in
+	         buttonM#connect#clicked ~callback:(fun () -> startMecaConfig window);
+            let imgMeca = GMisc.image ~height:240 ~packing:menuMeca#add () in
+            imgMeca#set_file "/usr/share/icons/gnome/48x48/categories/package_graphics.png";
+         let menuElec = GPack.vbox ~spacing:10 ~packing:frameContent#add () in
+            let buttonE = GButton.button
+	         ~label:"Électrocinétique"
+	         ~packing:menuElec#add () in
+	         buttonE#connect#clicked ~callback:(fun () -> startElecConfig window);
+            let imgElec = GMisc.image ~height:240 ~packing:menuElec#add () in
+            imgElec#set_file "/usr/share/icons/gnome/48x48/categories/package_graphics.png";
+         let quitButton = GButton.button
+         ~label:"Quitter"
+         ~packing:vbox#add () in
+            quitButton#connect#clicked ~callback:GMain.quit         
+;;
 
-let subTitle = GMisc.label ~markup:"<span font_size='large'>Veuillez choisir un type d'oscillateur :</span>" ~packing:vbox#add ()
-
-let frame = GBin.frame ~height:300 ~label:"Oscillateurs disponibles" ~packing:vbox#add ()
-let frameContent = GPack.hbox ~spacing:10 ~packing:frame#add ()
-let menuMeca = GPack.vbox ~spacing:10 ~packing:frameContent#add ()
-let menuElec = GPack.vbox ~spacing:10 ~packing:frameContent#add () 
-let buttonM = GButton.button
-	~label:"Mécanique"
-	~packing:menuMeca#add ()
-let imgMeca = GMisc.label ~text:"Img meca" ~height:240 ~packing:menuMeca#add ()
-let buttonE = GButton.button
-	~label:"Électrocinétique"
-	~packing:menuElec#add ()
-let imgElec = GMisc.label ~text:"Img elec" ~height:240 ~packing:menuElec#add ()
-let quitButton = GButton.button ~label:"Quitter" ~packing:vbox#add ()
+(* Main *)
 let _ =
   window#connect#destroy ~callback:GMain.quit;
+  menu window vbox;
   window#show ();
   GMain.main ()
+;;
